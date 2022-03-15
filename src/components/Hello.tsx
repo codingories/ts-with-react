02b044f8f64c483cb346a4f5c109e468275bb3a9
朱骏杰 <zhujunjie@sundear.com>
2022-03-15 14:16:55 +0800

    3-3第一次优化嗲吗

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const Hello = (props: any) => {
+// 定义传入的参数类型
+// 好处1.会有自动补全 2. 使用的时候获得验证
+interface IHelloProps {
+  message: string;
+}
+
+const Hello = (props: IHelloProps) => {
   return <h2>{props.message}</h2>;
 };
 
