67db6840e06c109fe486fbc65d3bee0e4ec22df9
朱骏杰 <zhujunjie@sundear.com>
2022-03-16 16:03:11 +0800

    3-7完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import LikeButton from './components/LikeButton'
+import MouseTracker from './components/MouseTracker'
+
 import './App.css';
 
 function App() {
@@ -11,7 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <LikeButton></LikeButton>
+        <MouseTracker></MouseTracker>
         <a
           className="App-link"
           href="https://reactjs.org"
