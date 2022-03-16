d9462ecfbe5ac2874deadb41b9fedff5987fe8dd
朱骏杰 <zhujunjie@sundear.com>
2022-03-16 15:29:08 +0800

    3-6完成

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,8 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const LikeButton: React.FC = () => {
   const [like, setLike] = useState(0);
   const [on, setOn] = useState(true);
+  // 默认情况下，第一次渲染完了和每次渲染之后都会执行，化繁为简就是说每一次渲染之后都会执行。
+  useEffect(() => {
+    document.title = `点击了${like}`;
+  });
   return (
     <>
       <button onClick={() => {setLike(like + 1);}}>
